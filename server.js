c01ad00c869c46bebd8e265b2ba237fdb0619027
tripti <triptiyadav61@gmail.com>
2026-09-17 18:16:59 +0530

    Add Server running on port

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.post("/message", (req, res) => {
         text: req.body.text
     });
 });
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
